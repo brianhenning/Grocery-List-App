0419634fde47e91c35eeb7fc62af855914816253
brianhenning <brian.henning.m@gmail.com>
2014-08-12 14:47:08 -0400

    jQuery Additions
    Added a majority of the jQuery functionality.

--- app.js
+++ app.js
@@ -2,10 +2,39 @@ $(document).ready(function(){
 	
 /*Add item when button is clicked*/
 
-	$("submit").click(function(){
-		var item = $("inputfield").val();
+	$('#submit').click(function(){
+		var item = $('#inputfield').val();
 		$('ul').append('<li>'+item+'</li>');
+		$('#inputfield').val('');
 	});
 
+	$(document).keyup(function(e){
+		if (e.which==13) {
+			$('#submit').click();
+		}
+	});
 
+/*Strike out list item when clicked*/
+
+	$(document).on("click", "li", function() {
+  		$(this).toggleClass("done");
+	});
+
+/*Remove list item when double clicked*/
+
+	$(document).on("dblclick", "li", function() {
+		$(this).fadeOut(400);
+	});
+
+/*Cursor hovers inside main div (interface)*/
+
+	$('#interface').mouseenter(function() {
+		$(this).css('background-color', '#E6CC80');
+	});
+
+/*Cursor leaves main div (interface)*/
+
+	$('#interface').mouseleave(function() {
+		$(this).css('background-color', '#CFB873');
+	});
 });
